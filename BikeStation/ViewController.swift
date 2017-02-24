51f09ca6124da26240ded58f000f79fcd971408e
kylegorter <kylegorter@Kyles-MacBook-Air.local>
2017-02-24 10:41:43 +0800

    Initial Commit

---
+++ BikeStation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BikeStation
+//
+//  Created by Kyle Gorter on 2/24/17.
+//  Copyright © 2017 Kyle Gorter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
