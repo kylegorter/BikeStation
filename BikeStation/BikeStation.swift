fa3d632dbaae50d86b0502320146f098735b230b
kylegorter <kylegorter@Kyles-MacBook-Air.local>
2017-02-24 17:11:17 +0800

    Second commit

--- BikeStation/BikeStation.swift
+++ BikeStation/BikeStation.swift
@@ -14,12 +14,46 @@ import CoreLocation
 
 class BikeStation {
     
+    var id: Int
+    
+    var latitude: Double
+    var longitude: Double
+    
+    var stationName: String
+    var availableBikes: Int
+    
+    var annotation: MKPointAnnotation!
+    
+    var route = MKRoute()
     
     
     
     
     
+    init (json: JSON) {
+        self.id = json["id"].int ?? 0
+        
+        self.latitude = json["latitude"].double ?? 0
+        self.longitude = json["longitude"].double ?? 0
+        
+        self.stationName = json["stationName"].string ?? ""
+        self.availableBikes = json["availableBikes"].int ?? 0
+        
+        annotation = MKPointAnnotation()
+        annotation.coordinate = self.coordinate()
+        annotation.title = stationName
+        annotation.subtitle = "Available Bikes: \(availableBikes)"
+        
+        
+        
+        
+    }
     
+    func coordinate() -> CLLocationCoordinate2D {
+        
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+    }
     
     
 }
--- BikeStation/ViewController.swift
+++ BikeStation/ViewController.swift
@@ -25,9 +25,9 @@ class ViewController: UIViewController {
     }
     
     
-    let locationManager = CLLocationManager()
+    let locationManager = CLLocationManager() // CLLocationManager is a class thus will use '='
     
-    var currentLocationAnno = MKAnnotation!.self
+    var currentLocationAnno: MKAnnotation! // MKAnnotation is a protocol thus will use ':'
     
     var stations = [BikeStation]()
     
@@ -43,52 +43,131 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        search.definesPresentationContext = true
+        definesPresentationContext = true
+        search.hidesNavigationBarDuringPresentation = false
+        search.dimsBackgroundDuringPresentation = false
+        
+        
+        
+        
         navigationItem.titleView = search.searchBar
         
-//        search.searchResultsUpdater = self
+        search.searchResultsUpdater = self
+        search.searchBar.delegate = self
+        
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
     
-    func searchFilter(searchText: String) {
-        
-        stations = []
-        
-        if searchText == "" {
-            stations = allStations
+    func fetchData(){
+        let url = "https://feeds.citibikenyc.com/stations/stations.json"
+        Alamofire.request(url).responseJSON(completionHandler: {(response) in
+            switch response.result {
+            case .success(let responseValue):
+                let json = JSON(responseValue)
+                for (key,subJson) : (String ,JSON) in json{
+                    if key == "stationBeanList"{
+                        for (_,subsubJson) : (String ,JSON) in subJson{
+                            let json = BikeStation(json: subsubJson)
+                            
+                            self.receiveRoute(sourceLocation: self.currentLocationAnno.coordinate, station: json)
+                            
+                        }
+                        
+                    }
+                }
+                
+                
+            case .failure(let error):
+                print(error.localizedDescription)
+                
+            }
             
-        } else {
-            return
-            
+        })
+        
+    }
+    
+    
+
+    func receiveRoute(sourceLocation: CLLocationCoordinate2D, station: BikeStation) {
+        
+        let startPlacemark = MKPlacemark(coordinate: sourceLocation, addressDictionary: nil)
+        
+        let finalPlacemark = MKPlacemark(coordinate: station.coordinate(), addressDictionary: nil)
+        
+        let startMapItem = MKMapItem(placemark: startPlacemark)
+        
+        let finalMapItem = MKMapItem(placemark: finalPlacemark)
+        
+        let requestDirection = MKDirectionsRequest()
+        requestDirection.source = startMapItem
+        requestDirection.destination = finalMapItem
+        requestDirection.transportType = .automobile
+        
+        let directions = MKDirections(request: requestDirection)
+        directions.calculate { (response, error) in
+            if let response = response {
+                if let route = response.routes.first {
+                    station.route = route
+                    self.stations.append(station)
+                    
+                }
+                
+            }
+          
+            self.allStations = self.stations
+            self.tableView.reloadData()
             
         }
         
     }
 
-
+    func searchController(searchText: String) {
+        
+        stations = []
+        
+        if searchText == "" {
+            
+            stations = allStations
+        } else {
+            
+            stations = allStations.filter { temp in
+                return (temp.stationName.lowercased().contains(searchText.lowercased()))
+        }
+        
+        tableView.reloadData()
+    }
+    
+}
 }
 
 extension ViewController : UISearchBarDelegate {
     
-    func searchBar(_ searchBar: UISearchController) {
-        
-        
+    func searchBar(_ searchBar: UISearchBar) {
+    
+        searchController(searchText: searchBar.text!)
     }
-
-    
-    
-    
-    
 }
 
 extension ViewController : UITableViewDelegate, UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return stations.count
         
     }
     
@@ -98,6 +177,11 @@ extension ViewController : UITableViewDelegate, UITableViewDataSource{
                 return UITableViewCell()
         }
         
+        let indexStations = stations[indexPath.row]
+        
+        cell.textLabel?.text = indexStations.stationName
+        let distance = String(format: "%0.3f", indexStations.route.distance/1000)
+        cell.detailTextLabel?.text = "Available bikes: \(indexStations.availableBikes)" + "Distance: \(distance)KM"
         // More data to be included
         
         return cell
@@ -117,11 +201,34 @@ extension ViewController: UISearchResultsUpdating {
         
         let searchBar = search.searchBar
         
-        
+        searchController(searchText: search.searchBar.text!)
     }
 
 }
 
+extension ViewController: CLLocationManagerDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            print("Locating")
+            if location.verticalAccuracy < 1000 && location.horizontalAccuracy < 1000 {
+                
+                locationManager.stopUpdatingLocation()
+                
+                currentLocationAnno = MKPointAnnotation()
+//                currentLocationAnno.coordinate = location.coordinate
+                
+            }
+    }
+        
+    
+    
+}
+}
 
 
 
